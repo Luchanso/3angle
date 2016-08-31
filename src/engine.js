3ba19e3952457bc77a3de3411d7d488c4c58f4db
luchanso <oleg@loutchansky.com>
2016-09-01 01:03:29 +0300

    Add keys to Enegine

--- src/engine.js
+++ src/engine.js
@@ -2,5 +2,13 @@ let Engine = {
   minWidth: 640,
   minHeight: 320,
   maxWidth: 1000,
-  maxHeight: 640
+  maxHeight: 676,
 };
+
+/**
+ * Sprite, Bitmap, etc... chache keys
+ * @type {Object}
+ */
+Engine.keys = {
+  triangle: 'triangle',
+}
