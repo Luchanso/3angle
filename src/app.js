049f0b7b351404fdefebb4f2d7adaee9b0828fda
luchanso <oleg@loutchansky.com>
2016-08-31 20:18:43 +0300

    Remove trash Engine variable in app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,7 @@
-let Engine = {
-  minWidth: 640,
-  minHeight: 320,
-  maxWidth: 1000,
-  maxHeight: 640
-};
-
 Engine.game = new Phaser.Game(Engine.maxWidth, Engine.maxHeight, Phaser.WEBGL);
 
 Engine.game.state.add('Boot', Engine.Boot);
-// Engine.game.state.add('Loader', Engine.Loader);
+Engine.game.state.add('Loader', Engine.Loader);
 // Engine.game.state.add('Menu', Engine.Menu);
 // Engine.game.state.add('Game', Engine.Game);
 // Engine.game.state.add('ScoreBoard', Engine.ScoreBoard);
