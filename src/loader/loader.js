2f39096e3f344d50725531fbbc153f34f4feefef
luchanso <oleg@loutchansky.com>
2016-08-31 20:36:56 +0300

    Remove trash in loader.js

--- src/loader/loader.js
+++ src/loader/loader.js
@@ -12,12 +12,7 @@ class Loader extends Phaser.State {
   }
 
   create() {
-
-  }
-
-  render() {
-    // this.game.debug.geom(new Phaser.Line(this.game.world.centerX, 0, this.game.world.centerX, this.game.height), 'rgba(43, 84, 238, 0.25)');
-    // this.game.debug.geom(new Phaser.Line(0, this.game.world.centerY, this.game.width, this.game.world.centerY), 'rgba(43, 84, 238, 0.25)');
+    this.state.start('Game');
   }
 
   addProgressLable() {
