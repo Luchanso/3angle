dd125dcbb95e509b64f49220b548e5b02acc3c6d
luchanso <oleg@loutchansky.com>
2016-09-01 01:04:04 +0300

    Finish triangle class

--- src/game/traingle.js
+++ src/game/traingle.js
@@ -1,10 +1,16 @@
-class Triange extends Phaser.Sprite {
-  constructor(x, y) {
-    super(x, y, Engine.game.cache.getBitmapData(Triangle.key));
+class Triangle extends Phaser.Sprite {
+  constructor(game, x, y, isRotated) {
+    super(game, x, y, Engine.game.cache.getBitmapData(Engine.keys.triangle));
 
+    this.width = Triangle.size;
+    this.height = Triangle.size;
     this.anchor.setTo(0.5);
+
+    if (isRotated) {
+      this.rotation = Math.PI;
+    }
   }
 }
 
-
-Triangle.key = 'traingle';
+Triangle.size = 65;
+Engine.Triangle = Triangle;
