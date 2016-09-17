860f5e074c3902485e8e4ab614a8aaa0bab73fb2
luchanso <oleg@loutchansky.com>
2016-09-17 19:02:22 +0300

    Add menu state :zzz:

--- src/app.js
+++ src/app.js
@@ -1,19 +1,11 @@
 Log.init();
 
-Log.write('app', 'Init game');
-
 Engine.game = new Phaser.Game(Engine.maxWidth, Engine.maxHeight, Phaser.AUTO);
-
-Log.write('app', 'Finish create game');
 
 Engine.game.state.add('Boot', Engine.Boot);
 Engine.game.state.add('Loader', Engine.Loader);
-// Engine.game.state.add('Menu', Engine.Menu);
+Engine.game.state.add('Menu', Engine.Menu);
 Engine.game.state.add('Game', Engine.Game);
 // Engine.game.state.add('ScoreBoard', Engine.ScoreBoard);
 
-Log.write('app', 'Finish adding state');
-
 Engine.game.state.start('Boot');
-
-Log.write('app', 'Run boot');
