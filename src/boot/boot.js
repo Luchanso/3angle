1b09b646aaef94d2c84ea2540aaae6a7dd13095c
luchanso <oleg@loutchansky.com>
2016-09-18 19:00:28 +0300

    Remove logger

--- src/boot/boot.js
+++ src/boot/boot.js
@@ -4,17 +4,12 @@ class Boot extends Phaser.State {
   }
 
   preload() {
-    Log.write('boot', 'Preloading');
   }
 
   create() {
-    Log.write('boot', 'Creating');
-
     this.game.stage.disableVisibilityChange = true;
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
-
-    Log.write('boot', 'Finish creating');
 
     this.state.start('Loader');
   }
