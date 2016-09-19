d0780babafc25a085fb63c930c9176fbd2750fd8
luchanso <oleg@loutchansky.com>
2016-09-19 13:36:24 +0300

    Add responsive canvas

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 Engine.game = new Phaser.Game(Engine.maxWidth, Engine.maxHeight, Phaser.AUTO);
 
+window.onresize = () => {
+  Engine.game.scale.setGameSize(window.innerWidth, window.innerHeight);
+};
+
 Engine.game.state.add('Boot', Engine.Boot);
 Engine.game.state.add('Loader', Engine.Loader);
 Engine.game.state.add('Menu', Engine.Menu);
--- src/engine.js
+++ src/engine.js
@@ -1,6 +1,6 @@
 let Engine = {
   minWidth: 640,
   minHeight: 320,
-  maxWidth: 1366,
-  maxHeight: 768,
+  maxWidth: window.innerWidth,
+  maxHeight: window.innerHeight,
 };
