4fd4d2d043aa29252fd8010cbab152d9213151ba
luchanso <oleg@loutchansky.com>
2016-09-21 13:03:33 +0300

    Add lvlpick to src/app.js

--- src/app.js
+++ src/app.js
@@ -5,9 +5,10 @@ window.onresize = () => {
 };
 
 Engine.game.state.add('Boot', Engine.Boot);
-Engine.game.state.add('Loader', Engine.Loader);
 Engine.game.state.add('Menu', Engine.Menu);
 Engine.game.state.add('Game', Engine.Game);
+Engine.game.state.add('Loader', Engine.Loader);
+Engine.game.state.add('Lvlpick', Engine.Lvlpick);
 Engine.game.state.add('Settings', Engine.Settings);
 // Engine.game.state.add('Leaderboard', Engine.Leaderboard);
 
