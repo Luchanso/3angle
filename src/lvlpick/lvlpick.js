6ec54dc382964eadfa30920c4fb8266b663d4424
luchanso <oleg@loutchansky.com>
2016-09-24 22:40:39 +0300

    Add some buttons to level select

--- src/lvlpick/lvlpick.js
+++ src/lvlpick/lvlpick.js
@@ -8,20 +8,53 @@ class Lvlpick extends Phaser.State {
   }
 
   create() {
+    this.animateBackground();
+
     this.createLable();
     this.createBtns();
   }
 
   render() {
-    // this.game.debug.geom(
-    //   new Phaser.Rectangle(
-    //     this.btnGroup.x,
-    //     this.btnGroup.y,
-    //     this.btnGroup.width,
-    //     this.btnGroup.height
-    //   ),
-    //   'rgba(53, 204, 61, 0.4)'
-    // );
+    let line1 = new Phaser.Line(this.game.world.centerX, 0, this.game.world.centerX, this.game.height);
+    let line2 = new Phaser.Line(0, this.game.world.centerY, this.game.width, this.game.world.centerY);
+
+    this.game.debug.geom(line1, 'rgb(0, 70, 255)');
+    this.game.debug.geom(line2, 'rgb(0, 70, 255)');
+  }
+
+  animateBackground() {
+    const targetColor = 0x0;
+    const targetRadius = Math.sqrt(Math.pow(this.game.width, 2) + Math.pow(this.game.height, 2));
+    const animationTime = 350;
+
+    let animation = {
+      graphics: this.add.graphics(0, 0),
+      _radius: 0,
+      game: this.game,
+    };
+
+    Object.defineProperty(animation, 'radius', {
+      get: function() { return this._radius; },
+      set: function(value) {
+        this._radius = value;
+
+        this.graphics.beginFill(targetColor);
+        this.graphics.drawCircle(this.game.world.centerX, this.game.world.centerY, this._radius);
+        this.graphics.endFill();
+      }
+    });
+
+    let tween = this.add.tween(animation)
+      .to({
+        radius: targetRadius
+      }, animationTime)
+      .start();
+
+    tween.onComplete
+      .add(() => {
+        this.stage.backgroundColor = targetColor;
+        animation.graphics.kill();
+      }, this);
   }
 
   createLable() {
@@ -30,15 +63,17 @@ class Lvlpick extends Phaser.State {
       fill: 'white'
     };
     const animationTime = 1000;
+    const animationDelay = 350;
+    const marginTop = 100;
 
     this.lable = this.add.text(
       this.game.world.centerX,
-      this.game.height / 8,
+      marginTop,
       'Select level',
       style
     );
 
-    this.lable.anchor.setTo(0.5);
+    this.lable.anchor.setTo(0.5, 0);
 
     this.lable.alpha = 0;
 
@@ -46,33 +81,42 @@ class Lvlpick extends Phaser.State {
       .to({
         alpha: 1
       }, animationTime)
+      .delay(animationDelay)
       .start();
   }
 
   createBtns() {
-    const marginLeft = 64;
-    const marginTop = 64;
-    const size = Engine.LvlpickBtn.size;
+    let testBtn = new Engine.LvlpickBtn(
+      this.game,
+      0,
+      this.lable.y + this.lable.height + 15,
+      0x00E5FF,
+      1,
+      2,
+    );
 
-    this.btnGroup = this.add.group();
+    let testBtn2 = new Engine.LvlpickBtn(
+      this.game,
+      0,
+      testBtn.y + testBtn.height + 15,
+      0x00E5FF,
+      2,
+      2,
+      true
+    );
 
-    for (let x = 0; x < 5; x++) {
-      for (let y = 0; y < 3; y++) {
-        let btn = new Engine.LvlpickBtn(
-          this.game,
-          x * (size + marginLeft),
-          y * (size + marginTop),
-          0xFFFFFF,
-          3,
-          false
-        );
+    let testBtn3 = new Engine.LvlpickBtn(
+      this.game,
+      0,
+      testBtn2.y + testBtn2.height + 15,
+      0x00E5FF,
+      3,
+      3
+    );
 
-        this.btnGroup.add(btn);
-      }
-    }
-
-    this.btnGroup.x = this.game.width / 2 - this.btnGroup.width / 2;
-    this.btnGroup.y = this.game.height / 2 - this.btnGroup.height / 2;
+    this.add.existing(testBtn);
+    this.add.existing(testBtn2);
+    this.add.existing(testBtn3);
   }
 }
 
