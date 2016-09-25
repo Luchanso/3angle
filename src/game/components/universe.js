20602ceb8d1921b3d3ec9c95a4a675ff9c0214a5
luchanso <oleg@loutchansky.com>
2016-09-25 21:44:48 +0300

    Change numbers star and change star form from rect to circle and close #47 issue

--- src/game/components/universe.js
+++ src/game/components/universe.js
@@ -13,17 +13,17 @@ class Universe extends Phaser.Graphics {
     const maxStarSize = 4;
     const minStarSize = 1;
     const universeSize = Math.sqrt(Math.pow(window.screen.availWidth, 2) + Math.pow(window.screen.availHeight, 2));
-    const stars = 100;
+    const stars = 500;
 
     for (var i = 0; i < stars; i++) {
       const whiteGradation = 255 - this.game.rnd.between(0, 32);
       const starColor = Phaser.Color.getColor(whiteGradation, whiteGradation, whiteGradation);
       let x = this.game.rnd.between(-universeSize / 2, universeSize / 2);
       let y = this.game.rnd.between(-universeSize / 2, universeSize / 2);
-      let startSize = this.game.rnd.between(minStarSize, maxStarSize);
+      let starSize = this.game.rnd.between(minStarSize, maxStarSize);
 
-      this.beginFill(starColor);
-      this.drawRect(x, y, startSize, startSize);
+      this.beginFill(starColor, Math.random());
+      this.drawCircle(x, y, starSize);
       this.endFill();
     }
   }
