ebe18858276ea3842db4c9dfe369437c011078bb
luchanso <oleg@loutchansky.com>
2016-09-25 21:02:15 +0300

    Change lvlpick button style

--- src/lvlpick/components/lvlpick-btn.js
+++ src/lvlpick/components/lvlpick-btn.js
@@ -1,91 +1,46 @@
 class LvlpickBtn extends Phaser.Graphics {
-  constructor(game, x, y, color, numberPosition, countStars, isLock = false) {
+  constructor(game, x, y, number, isLock) {
     super(game, x, y);
 
-    this.data.color = color;
-    this.data.countStars = countStars;
     this.data.isLock = isLock;
-    this.data.numberPosition = numberPosition;
-    this.data.defaultColor = 0xE0E0E0;
-    this.data.lockColor = 0x9E9E9E;
+    this.data.numberPosition = number;
 
-    this.triangleSize = 47;
-
-    this.drawTriangles();
-    this.addNumberPosition();
+    this.drawBtn();
+    this.addText();
   }
 
-  drawTriangles() {
-    const maxTriangles = 3;
-    const margin = 15;
-    const size = this.triangleSize;
-    const summSize = size + (margin + size / 2) * (maxTriangles - 1);
+  drawBtn() {
+    const size = LvlpickBtn.size;
+    const activeColor = 0xCFD8DC;// 0x9d9d9d;
+    const lockedColor = 0x546E7A;
 
-    for (let i = 0; i < maxTriangles; i++) {
-      let color = this.data.defaultColor;
-      let x = this.game.world.centerX - summSize / 2 + (size / 2 + margin) * i;
-      let y = 0;
-      let isOdd = i % 2 !== 0;
+    let color;
 
-      if (this.data.numberPosition % 2 === 0) {
-        isOdd = !isOdd;
-      }
-
-      if (this.data.isLock) {
-        color = this.data.lockColor;
-      }
-      else if (i < this.data.countStars) {
-        color = this.data.color;
-      }
-
-      this.drawTriangle(x, y, color, isOdd);
+    if (this.data.isLock) {
+      color = lockedColor;
+    } else {
+      color = activeColor;
     }
-  }
-
-  drawTriangle(x, y, color, isRotated = false) {
-    const size = this.triangleSize;
 
     this.beginFill(color);
-
-    if (isRotated) {
-      this.moveTo(x, y);
-      this.lineTo(x + size / 2, y + size);
-      this.lineTo(x + size, y);
-      this.lineTo(x, y);
-    } else {
-      this.moveTo(x, y + size);
-      this.lineTo(x + size, y + size);
-      this.lineTo(x + size / 2, y);
-      this.lineTo(x, y + size);
-    }
-
+    this.drawRect(0, 0, size, size);
     this.endFill();
   }
 
-  addNumberPosition() {
-    const marginRight = 100;
+  addText() {
+    const size = LvlpickBtn.size;
     const style = {
-      font: '27px Open Sans',
-      fontWeight: 'bold',
-      fill: Phaser.Color.getWebRGB(this.data.color)
+      font: '23px Open Sans',
+      fill: 'black'
     };
-    const x = this.game.world.centerX - marginRight;
-    const y = this.triangleSize / 2;
 
-    if (this.data.isLock) {
-      style.fill = Phaser.Color.getWebRGB(this.data.lockColor);
-    }
-
-    this.lable = this.game.make.text(
-      x,
-      y,
-      this.data.numberPosition + '.',
-      style
-    );
+    this.lable = this.game.make.text(size / 2, size / 2, this.data.numberPosition, style);
     this.lable.anchor.setTo(0.5);
 
     this.addChild(this.lable);
   }
 }
+
+LvlpickBtn.size = 75;
 
 Engine.LvlpickBtn = LvlpickBtn;
