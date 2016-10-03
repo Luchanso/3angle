1481e3bc78d46f3ab5ffbf7f5a6535ef02a761fc
luchanso <oleg@loutchansky.com>
2016-10-03 22:16:49 +0300

    Add animation 3d function to illuminati class

--- src/game/components/illuminati.js
+++ src/game/components/illuminati.js
@@ -1,23 +1,116 @@
-class Illuminati extends Phaser.Graphics {
+class Illuminati extends Phaser.Sprite {
   constructor(game, x = 0, y = 0) {
-    super(game, x, y);
+    const ratioSize = 0.4;
 
-    this.reDraw();
+    super(game, x, y, 'illuminati-original', 0);
+
+    this.create3DAnimation();
+    this.createActionTimer();
+
+    this.width *= ratioSize;
+    this.height *= ratioSize;
   }
 
-  reDraw() {
-    const size = 100;
+  create3DAnimation() {
+    this.sprite3DLeft = this.game.make.sprite(0, 0, 'illuminati3d-1');
+    this.sprite3DLeft.alpha = 0;
 
-    this.lineStyle(5, 0x00E676, 1);
-    this.moveTo(0, size);
-    this.lineTo(size, size);
-    this.lineTo(size / 2, 0);
-    this.lineTo(0, size);
+    this.sprite3DRight = this.game.make.sprite(0, 0, 'illuminati3d-2');
+    this.sprite3DRight.alpha = 0;
 
-    // this.lineStyle(5, 0x00E676, 1);
-    this.drawEllipse(50, 60, 22, 10);
-    this.drawEllipse(50, 54, 6, 4);
-    // this.endFill();
+    this.addChild(this.sprite3DLeft);
+    this.addChild(this.sprite3DRight);
+  }
+
+  createActionTimer() {
+    const actionDelay = 10000;
+
+    this.actionTimer = this.game.time.create();
+    this.actionTimer.loop(actionDelay, this.action, this);
+    this.actionTimer.start();
+  }
+
+  action() {
+    const actions = [
+      this.animate3D,
+    ];
+
+    let callbackAction = this.game.rnd.pick(actions);
+
+    callbackAction.bind(this)();
+  }
+
+  animate3D() {
+    const maxOffsetX = 45;
+    const maxOffsetY = 5;
+    const animationTime = 1000;
+    const targetAlpha = 0.5;
+    const animationFunc = Illuminati.animation3DEasing
+
+    this.game.tweens.removeFrom(this.sprite3DRight);
+    this.game.tweens.removeFrom(this.sprite3DLeft);
+
+    this.game.add.tween(this.sprite3DLeft)
+      .to({
+        x: this.sprite3DLeft.x - maxOffsetX,
+        y: this.sprite3DLeft.y - maxOffsetY,
+        alpha: targetAlpha
+      }, animationTime, animationFunc)
+      .to({
+        x: this.sprite3DLeft.x,
+        y: this.sprite3DLeft.y,
+        alpha: 0
+      }, animationTime, animationFunc)
+      .start();
+
+    this.game.add.tween(this.sprite3DRight)
+      .to({
+        x: this.sprite3DRight.x + maxOffsetX,
+        y: this.sprite3DRight.y + maxOffsetY,
+        alpha: targetAlpha
+      }, animationTime, animationFunc)
+      .to({
+        x: this.sprite3DRight.x,
+        y: this.sprite3DRight.y,
+        alpha: 0
+      }, animationTime, animationFunc)
+      .start();
+  }
+
+  static animation3DEasing(time) {
+      let result = 0;
+      let min = 0;
+      let max = 0;
+
+      if (time < 0.1) {
+        min = 0; max = 0.1;
+      } else if (time < 0.2) {
+        min = 0.1; max = 0.2;
+      } else if (time < 0.3) {
+        min = 0.1; max = 0.7;
+      } else if (time < 0.4) {
+        min = 0.1; max = 0.35;
+      } else if (time < 0.5) {
+        min = 0.3; max = 0.67;
+      } else if (time < 0.6) {
+        min = 0.4; max = 0.8;
+      } else if (time < 0.7) {
+        min = 0.5; max = 1;
+      } else if (time < 0.8) {
+        min = 0.65; max = 1;
+      } else if (time < 0.9) {
+        min = 0.7; max = 1;
+      } else {
+        min = 0.95; max = 1.5;
+      }
+
+      result = min + Math.random() * max;
+
+      return result;
+  }
+
+  changeColor(color) {
+
   }
 }
 
